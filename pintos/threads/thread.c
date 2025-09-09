3544ef8e9a9dfd92042a75b672f6beb975bf7bb9
EPSIHYEON <127606365+EPSIHYEON@users.noreply.github.com>
2025-09-09 21:40:51 +0900

    우선순위[5]

--- pintos/threads/thread.c
+++ pintos/threads/thread.c
@@ -41,7 +41,7 @@ static struct thread *initial_thread;
 /* Lock used by allocate_tid(). */
 static struct lock tid_lock;
 
-static bool schedule_enable = false;
+
 
 /* Thread destruction requests */
 static struct list destruction_req;
@@ -133,13 +133,14 @@ thread_start (void) {
 	sema_init (&idle_started, 0);
 	thread_create ("idle", PRI_MIN, idle, &idle_started);
 
-	/* Start preemptive thread scheduling. */
-	intr_enable ();
+
 
 	/* Wait for the idle thread to initialize idle_thread. */
 	sema_down (&idle_started);
 
-	schedule_enable = true;
+	/* Start preemptive thread scheduling. */
+	intr_enable ();
+
 }
 
 /* Called by the timer interrupt handler at each timer tick.
@@ -252,7 +253,7 @@ thread_unblock (struct thread *t) {
 	list_insert_ordered(&ready_list, &t->elem, compare_less, NULL);
 	t->status = THREAD_READY;
 
-	if (schedule_enable && t->priority > thread_current ()->priority) {
+	if (t->priority > thread_current ()->priority) {
 	  if (intr_context()) {
 		  intr_yield_on_return ();
 		}else {
